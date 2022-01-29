5932e004188fcb553a832d7bbe7e36f702edb915
Shivendra Singh <92018084+Shivendra-Singh-Code@users.noreply.github.com>
2022-01-29 21:42:15 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -4,16 +4,17 @@ $host = "frtsql.mysql.database.azure.com";
 $user = "letsquizad";
 $pass = "King1234";
 $db   = "quiz";
-$conn = null;
+//$conn = null;
 
 
 
-$options = array(PDO::MYSQL_ATTR_SSL_CA => '../BaltimoreCyberTrustRoot.crt.pem');
+$options = array(PDO::MYSQL_ATTR_SSL_CA => '../DigiCertGlobalRootCA.crt.pem');
 try {
-  $conn = new PDO("mysql:host={$host};dbname={$db};",$user,$pass,$options);
+  $conn = new PDO("mysql:host={$host};port=3306;dbname={$db};",$user,$pass,$options);
 } catch (Exception $e) {
   
 }
 
 
- ?>
+
+?>
