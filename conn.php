48bcf2f9716f6437216b5f5a59b2017eacf62333
Shivendra Singh <92018084+Shivendra-Singh-Code@users.noreply.github.com>
2022-01-29 20:26:33 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -7,12 +7,15 @@ $db   = "quiz";
 $conn = null;
 
 
-$options = array(DigiCertGlobalRootCA.crt.pem);
+$conn = mysqli_init(); mysqli_ssl_set($con,NULL,NULL, "../SSL/DigiCertGlobalRootCA.crt.pem", NULL, NULL); 
+mysqli_real_connect($conn, $host, $user, $pass, $db, 3306, MYSQLI_CLIENT_SSL);
+
+/*$options = array(DigiCertGlobalRootCA.crt.pem);
 try {
   $conn = new PDO("mysql:host={$host};dbname={$db};",$user,$pass,$options);
 } catch (Exception $e) {
   
-}
+}*/
 
 
  ?>
