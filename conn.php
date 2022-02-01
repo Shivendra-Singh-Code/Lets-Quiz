c6172f9f802691b97a897e0fbf2d99b6d5c8b5a4
Shivendra Singh <92018084+Shivendra-Singh-Code@users.noreply.github.com>
2022-02-01 18:20:25 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -8,10 +8,8 @@ $db   = "quiz1";
 
 
 
-try {
-  $conn = new PDO("mysql:host={$host};dbname={$db};",$user,$pass);
-} catch (Exception $e) {
-  
+$conn = mysqli_init(); mysqli_ssl_set($con,NULL,NULL, "../DigiCertGlobalRootCA.crt.pem", NULL, NULL); 
+mysqli_real_connect($conn, "frtsql.mysql.database.azure.com", "letsquizad", "Shivendra@23", "quiz1", 3306, MYSQLI_CLIENT_SSL);
 
 
 
